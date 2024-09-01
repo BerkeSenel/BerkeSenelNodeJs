54b9a0f3a080380f9d8633cdf2c447e8c80e5c2d
BerkeSenel <berke99senel99@hotmail.com>
2024-09-02 00:13:29 +0300

    Add crud operations to services

--- src/services/books.js
+++ src/services/books.js
@@ -1,28 +1,33 @@
-
-
+const repository = require('../repositories/book')
 exports.getAll = async function (req, res) {
-    try{
-
-    }catch(err){
-
+    try {
+        const allBooks = await repository.getAll()
+        return allBooks
+    } catch (err) {
+        console.log("Error on getAll books : ",err)
+        throw err
     }
   
 }
 
-exports.getOne = async function (req, res) {
-    try{
-
-    }catch(err){
-
+exports.getOne = async function (id) {
+    try {
+        const user = await repository.getOne(id)
+        return user
+    } catch (err) {
+        console.log("Error on getOne book : ",err)
+        throw err
     }
-  
-  }
+}
 
-exports.create = async function (req, res) {
-    try{
-
-    }catch(err){
-
+exports.create = async function (body) {
+    try {
+        const { name } = body;
+        const newBookObject = { name }
+        const newBook = await repository.create(newBookObject)
+        return newBook
+    } catch (err) {
+        console.log("Error on creating book : ",err)
+        throw err
     }
-  
 }
--- src/services/users.js
+++ src/services/users.js
@@ -1,31 +1,36 @@
-
+const repository = require('../repositories/user')
 exports.getAll = async function (req, res) {
     try {
-
+        const allUsers = await repository.getAll()
+        return allUsers
     } catch (err) {
-        
+        console.log("Error on getAll user : ",err)
+        throw err
     }
 
 }
 
-exports.getOne = async function (req, res) {
+exports.getOne = async function (id) {
     try {
-        
-       
+        const user = await repository.getOne(id)
+        return user
     } catch (err) {
-        
+        console.log("Error on getOne user : ",err)
+        throw err
     }
 
 }
 
-exports.create = async function (req, res) {
+exports.create = async function (body) {
     try {
-        
-       
+        const { name } = body;
+        const newUserObject = { name }
+        const newUser = await repository.create(newUserObject)
+        return newUser
     } catch (err) {
-        
+        console.log("Error on creating user : ",err)
+        throw err
     }
-
 }
 
 exports.borrowBook = async function (req, res) {
