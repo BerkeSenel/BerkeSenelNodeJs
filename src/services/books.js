a92fe900527b051070a13d1ecd2fff0ea99ee36c
BerkeSenel <berke99senel99@hotmail.com>
2024-09-01 22:44:39 +0300

    Add Services

---
+++ src/services/books.js
@@ -0,0 +1,28 @@
+
+
+exports.getAll = async function (req, res) {
+    try{
+
+    }catch(err){
+
+    }
+  
+}
+
+exports.getOne = async function (req, res) {
+    try{
+
+    }catch(err){
+
+    }
+  
+  }
+
+exports.create = async function (req, res) {
+    try{
+
+    }catch(err){
+
+    }
+  
+}
---
+++ src/services/users.js
@@ -0,0 +1,49 @@
+
+exports.getAll = async function (req, res) {
+    try {
+
+    } catch (err) {
+        
+    }
+
+}
+
+exports.getOne = async function (req, res) {
+    try {
+        
+       
+    } catch (err) {
+        
+    }
+
+}
+
+exports.create = async function (req, res) {
+    try {
+        
+       
+    } catch (err) {
+        
+    }
+
+}
+
+exports.borrowBook = async function (req, res) {
+    try {
+        
+       
+    } catch (err) {
+        
+    }
+
+}
+
+exports.returnBook = async function (req, res) {
+    try {
+        
+       
+    } catch (err) {
+        
+    }
+
+}
