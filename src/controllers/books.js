099714e64815b05219188f5a284656bf475a8bb0
BerkeSenel <berke99senel99@hotmail.com>
2024-09-02 00:13:03 +0300

    Update crud operation controllers

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,29 +1,30 @@
 const service = require('../services/books')
 
 exports.getAll = async function (req, res) {
-  try{
-    const books = await service.getAll();
-    return res.status(200).send(games);
-  }catch(err){
-    return res.status(400).json({error:err})
+  try {
+    const data = await service.getAll();
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(400).json({ error: err.toString() })
   }
 
 }
 
 exports.getOne = async function (req, res) {
-    try{
-      const book = await service.getOne();
-      return res.status(200).send(game);
-    }catch(err){
-      return res.status(400).json({error:err})
-    }
-  
+  try {
+    const { id } = req.params
+    const data = await service.getOne(id);
+    return res.status(200).send(data);
+  } catch (err) {
+    return res.status(400).json({ error: err.toString() })
   }
+}
 
 exports.create = async function (req, res) {
     try{
-      const newBook = await service.create();
-      return res.status(200).send(newBook);
+      const { body } = req
+      const data = await service.create(body);
+      return res.status(200).send(data);
     }catch(err){
       console.log("err : ",err)
       return res.status(400).json({error:err})
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,50 +2,51 @@ const service = require('../services/users')
 
 exports.getAll = async function (req, res) {
   try {
-    const datas = await service.latest();
-    return res.status(200).send(datas);
+    const data = await service.getAll();
+    return res.status(200).send(data);
   } catch (err) {
-    return res.status(400).json({ error: err })
+    return res.status(400).json({ error: err.toString() })
   }
 
 }
 
 exports.getOne = async function (req, res) {
   try {
-    const datas = await service.mostDownloaded();
-    return res.status(200).send(datas);
+    const { id } = req.params
+    const data = await service.getOne(id);
+    return res.status(200).send(data);
   } catch (err) {
-    return res.status(400).json({ error: err })
+    return res.status(400).json({ error: err.toString() })
   }
-
 }
 
 exports.create = async function (req, res) {
   try {
-    const datas = await service.mostDownloaded();
-    return res.status(200).send(datas);
+    const { body } = req
+    const data = await service.create(body);
+    return res.status(200).send(data);
   } catch (err) {
-    return res.status(400).json({ error: err })
+    return res.status(400).json({ error: err.toString() })
   }
 
 }
 
 exports.borrowBook = async function (req, res) {
   try {
-    const datas = await service.mostDownloaded();
-    return res.status(200).send(datas);
+    const data = await service.borrowBook();
+    return res.status(200).send(data);
   } catch (err) {
-    return res.status(400).json({ error: err })
+    return res.status(400).json({ error: err.toString() })
   }
 
 }
 
 exports.returnBook = async function (req, res) {
   try {
-    const datas = await service.mostDownloaded();
-    return res.status(200).send(datas);
+    const data = await service.returnBook();
+    return res.status(200).send(data);
   } catch (err) {
-    return res.status(400).json({ error: err })
+    return res.status(400).json({ error: err.toString() })
   }
 
 }
