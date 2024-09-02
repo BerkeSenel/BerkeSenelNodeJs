9be971b770d985a7a18ca4627399b3553a29b851
BerkeSenel <berke99senel99@hotmail.com>
2024-09-02 04:14:23 +0300

    update repositories

--- src/repositories/book.js
+++ src/repositories/book.js
@@ -1,11 +1,13 @@
 const models = require('../models');
 const Book = models.Book
+const User = models.User
+const UserBook = models.UserBook
 exports.create = async function ({ name }) {
     try {
         const newBook = await Book.create({ name });
         return newBook
     } catch (err) {
-        console.log("Error on repository create book : ",err)
+        console.log("Error on repository create book : ", err)
         throw err
     }
 }
@@ -15,7 +17,7 @@ exports.getAll = async function () {
         const allBooks = await Book.findAll();
         return allBooks
     } catch (err) {
-        console.log("Error on repository getAll book : ",err)
+        console.log("Error on repository getAll book : ", err)
         throw err
     }
 }
@@ -26,7 +28,64 @@ exports.getOne = async function (id) {
         if (!book) throw "Book Not Found"
         return book
     } catch (err) {
-        console.log("Error on repository getOne Book: ",err)
+        console.log("Error on repository getOne Book: ", err)
+        throw err
+    }
+}
+
+exports.getBookWithUsers = async function (id) {
+    try {
+        const book = await Book.findOne(
+            {
+                where: { id },
+                include: [
+                    {
+                        model: User,
+                        through: { 
+                            model: UserBook
+                          },
+                    }
+                ],
+            }
+        );
+        if (!book) throw "Book Not Found"
+        return book;
+    } catch (err) {
+        console.log("Error on repository getBookWithUsers user: ", err)
+        throw err
+    }
+}
+
+exports.getBookWithAllUsers = async function (id) {
+    try {
+        const userBooks = await UserBook.findAll({
+            where: { BookId: id }, // Fetch by book ID
+            paranoid: false, // Include soft-deleted UserBooks if needed
+          });
+        if (!userBooks.length) throw "Book not borrowed by the user"
+        return userBooks;
+    } catch (err) {
+        console.log("Error on repository getBookWithAllUsers user: ", err)
+        throw err
+    }
+}
+
+exports.updateBookById = async function (id, updateObject, t) {
+    try {
+        const query = {
+            where: {
+                id,
+            }
+        }
+        if (t) {
+            query.transaction = t
+        }
+        await Book.update(
+            updateObject,
+            query
+        );
+    } catch (err) {
+        console.log("Error on repository updateBookById user: ", err)
         throw err
     }
 }
--- src/repositories/user.js
+++ src/repositories/user.js
@@ -1,5 +1,7 @@
 const models = require('../models');
 const User = models.User
+const Book = models.Book
+const UserBook = models.UserBook
 
 exports.create = async function ({ name }) {
     try {
@@ -21,10 +23,65 @@ exports.getAll = async function () {
     }
 }
 
+exports.getOneUserWithAllBooks = async function (id) {
+    try {
+        /*
+        If the user returned a book that he borrowed before and then borrowed it again, 
+        I was caught in the unique problem here. 
+        I continued with the code below in case this happens.
+        const user = await User.findOne(
+            {
+                 where: { id },
+                 include: [
+                    {
+                        model: Book,
+                        through: { 
+                            model: UserBook,
+                            paranoid:false
+                          },
+                    }
+                ], 
+            }
+        );
+        */
+        const user = await User.findOne({where: { id },});
+        if (!user) throw "User Not Found"
+        const userBooks = await UserBook.findAll({
+            where: { UserId: id },
+            include: [
+              {
+                model: Book,
+                paranoid: false, 
+              },
+            ],
+            paranoid: false,
+          });
+          const userWithBooks = {
+            ...user.toJSON(), 
+            userBooks: userBooks.map(ub => ub.toJSON()) 
+          };
+        return userWithBooks
+    } catch (err) {
+        console.log("Error on repository getOneUserWithAllBooks user: ",err)
+        throw err
+    }
+}
+
 exports.getOne = async function (id) {
     try {
-        const user = await User.findOne({ where: { id } });
-        if (!user) throw "User Not Found"
+        const user = await User.findOne(
+            {
+                 where: { id },
+                 include: [
+                    {
+                        model: Book,
+                        through: { 
+                            model: UserBook,
+                          },
+                    }
+                ], 
+            }
+        );
         return user
     } catch (err) {
         console.log("Error on repository getOne user: ",err)
