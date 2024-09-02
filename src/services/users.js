cba4b09b0301c172f846e7567d6f1274fb1adb76
BerkeSenel <berke99senel99@hotmail.com>
2024-09-02 04:14:55 +0300

    update user service

--- src/services/users.js
+++ src/services/users.js
@@ -1,4 +1,7 @@
 const repository = require('../repositories/user')
+const bookRepository = require('../repositories/book')
+const models = require('../models');
+const sequelize = models.sequelize
 exports.getAll = async function (req, res) {
     try {
         const allUsers = await repository.getAll()
@@ -12,7 +15,7 @@ exports.getAll = async function (req, res) {
 
 exports.getOne = async function (id) {
     try {
-        const user = await repository.getOne(id)
+        const user = await repository.getOneUserWithAllBooks(id)
         return user
     } catch (err) {
         console.log("Error on getOne user : ",err)
@@ -33,22 +36,40 @@ exports.create = async function (body) {
     }
 }
 
-exports.borrowBook = async function (req, res) {
+exports.borrowBook = async function (userId, bookId) {
     try {
-        
-       
+        const user = await repository.getOne(userId)
+        const book = await bookRepository.getBookWithUsers(bookId)
+        if (book.Users.length) throw "The book has already been borrowed";
+        await user.addBook(book);
     } catch (err) {
-        
+        console.log("Error on borrowing book : ",err)
+        throw err
     }
-
 }
 
-exports.returnBook = async function (req, res) {
+exports.returnBook = async function (userId, bookId, score) {
     try {
-        
-       
+        const user = await repository.getOne(userId)
+        const bookUsers = await bookRepository.getBookWithAllUsers(bookId);
+        const foundBook = user.Books.find((item) => item.id === parseInt(bookId));
+        if(!foundBook) throw "User does not have this book "
+        await sequelize.transaction(async t => {
+            let totalRating = score
+            bookUsers.forEach(element => {
+                totalRating += element.rating || 0
+             });
+             const avgRating = totalRating / bookUsers.length
+             foundBook.UserBook.rating = score
+             await foundBook.UserBook.save({ transaction: t });
+             await bookRepository.updateBookById(bookId, {avgRating: avgRating}, t)
+             await user.removeBook(
+                foundBook,
+              { transaction: t },
+            );
+          });
     } catch (err) {
-        
+        console.log("Error on returnBook book : ",err)
+        throw err
     }
-
 }
